5e20fdabf3ac633a1228cc71395f72aa0697b5c9
Thibaultferre <thibault.ferre@isep.fr>
2017-10-16 20:53:36 +0200

    Create ultrasonic_function.py

---
+++ ultrasonic_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+
+def client_send_data(host,port,data):
+
+    import socket
+    hote = host
+    ports =port
+
+    socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.connect((hote, port))
+    print("Connection on {}".format(port))
+
+    socket.send(data.encode())
+
+    print("Close")
+    socket.close()
