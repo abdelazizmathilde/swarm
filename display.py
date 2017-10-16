ac6b99467cdfb5696ad19f3d8fb847a0fb441c6c
Thibaultferre <thibault.ferre@isep.fr>
2017-10-16 20:54:04 +0200

    Create display.py

---
+++ display.py
@@ -0,0 +1,6 @@
+def display_data_swarm(table):
+    print("-------------------------------------------\n")
+    print("--------------- Data of Swarm 1n ------------------\n")
+    for i in range(len(table)):
+        print(table[i]+"\n")
+    print("-------------------------------------------\n")
