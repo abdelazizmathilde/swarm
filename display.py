1309c3dbd10deb47b37d65046d341ae5e9c472f4
Thibaultferre <thibault.ferre@isep.fr>
2017-10-19 14:45:14 +0200

    Delete display.py

--- display.py
+++
@@ -1,7 +0,0 @@
-def display_data_swarm(table):
-    print("-------------------------------------------\n")
-    print("--------------- Data of Swarm    aaaa ------------------\n")
-    for i in range(len(table)):
-        print(table[i]+"\n")
-    print("-------------------------------------------\n")
-
