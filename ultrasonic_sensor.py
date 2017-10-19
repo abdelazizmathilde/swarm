2b0eae054da8a116965b4c95fda8757012d15446
Thibaultferre <thibault.ferre@isep.fr>
2017-10-19 14:44:25 +0200

    Delete ultrasonic_sensor.py

--- ultrasonic_sensor.py
+++
@@ -1,8 +0,0 @@
-def mesure_distance(sensor, distance):
-    if distance == 'distance':
-        sensor.mode = 'US-DIST-CM'
-        units = sensor.units
-
-    mesure = sensor.value() / 10  # convert mm to cm
-
-    return mesure
