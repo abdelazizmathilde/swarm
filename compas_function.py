6674d6063a5afd52276199403a48f754e7d6f89c
Thibaultferre <thibault.ferre@isep.fr>
2017-10-19 14:45:23 +0200

    Delete compas_function.py

--- compas_function.py
+++
@@ -1,7 +0,0 @@
-def mesure_position(sensor,mode):
-    if mode == 'angle':
-        sensor.mode = 'GYRO-ANG'
-        units = sensor.units #choose the degres units
-        mesure = sensor.value()
-
-        return mesure
