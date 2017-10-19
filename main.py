c14c755df0794a922d874a57087d0f47f326ca71
Thibaultferre <thibault.ferre@isep.fr>
2017-10-19 14:43:11 +0200

    Swarm_bronze

--- main.py
+++ main.py
@@ -1,68 +1,24 @@
 
+import sys
 from ev3dev.ev3 import *
-from time import sleep
-import threading
-import urllib.request
-import socket
-from ultrasonic_function import *
-from compas_function import *
-from client import *
+import time
+from ultrasonic_sensor import *
 
 
-mA = LargeMotor('outA')
-mB = LargeMotor('outB')
 ultrasonic = UltrasonicSensor()
 assert ultrasonic.connected
-gyroscope = GyroSensor()
-assert gyroscope.connected
 
 
-def findeboucle():
-    global encore
-    encore = False
+while True:
 
-
-encore = True
-
-timer = threading.Timer(16, findeboucle)
-timer.start()
-
-data = []
-distance_last = 0
-while encore:
-    mA.run_forever(speed_sp=400)
-    mB.run_forever(speed_sp=400)
 
     distance = mesure_distance(ultrasonic,'distance')
+    if distance >= 100:
 
-    if distance <= 7:
+        distance = "inf-"
 
-        if distance != distance_last:
-
-            angle = mesure_position(gyroscope,'angle')
-
-            value = "Distance between robot and obstacle is:"+ str(distance) + " with an angle of :"+ str(+angle)
-            data.append(value)
-            distance_last = distance
-
-
-    mA.stop(stop_action="hold")
-    mB.stop(stop_action="hold")
-
-    mB.run_to_rel_pos(position_sp=360, speed_sp=900, stop_action="hold")
-
-
-
-
-
-mA.stop(stop_action="brake")
-mB.stop(stop_action="brake")
-
-for i in range(len(data)):
-    value="Swarm1 ->"+ str(data[i])
-    print(value)
-    client_send_data('localhost', 1111,value)
-
+    print(distance, end="\r")
+    time.sleep(0.5)
 
 
 
